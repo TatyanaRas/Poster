7f701204fa38eeef46b85358ab034d085663ced3
Tatyana <tatyanalipch@yandex.ru>
2024-06-08 00:15:56 +0300

    src/

--- src/main/java/AddingPosterManager.java
+++ src/main/java/AddingPosterManager.java
@@ -3,13 +3,17 @@ public class AddingPosterManager {
     private int limit;
     private String[] names = new String[0];
 
-    public  AddingPosterManager() {
-        limit = 5;
+    public AddingPosterManager() {
+        limit = 5;//?
     }
 
-      public void AddingPosterManager(String newName) {
+    public AddingPosterManager(int limit) {
+        this.limit = limit;
+    }
 
-   //  String[] names = new String[];
+    public void AddingPosterManager(String newName) {
+
+        //  String[] names = new String[];
 
         String[] tmp = new String[names.length + 1];
         for (int i = 0; i < names.length; i++) {
@@ -23,13 +27,23 @@ public class AddingPosterManager {
     public String[] findAll() {
         return names;
     }
+
+
+    public String[] findLast() {
+        int resultLength;
+        if (names.length < limit) {
+            resultLength = names.length;
+        } else {
+            resultLength = limit;
+        }
+        String[] tmp = new String[resultLength];
+        for (int i = 0; i < tmp.length; i++) {
+
+            tmp[i] = names[names.length - 1 - i];
+            // names = tmp;
+        }
+        return tmp;
+
+    }
 }
-
-
- //  public String[] findLast() {
-
-
-
- //  }
-
 
--- src/test/java/AddingPosterManagerTest.java
+++ src/test/java/AddingPosterManagerTest.java
@@ -3,34 +3,109 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class AddingPosterManagerTest {
-@BeforeEach
+    @BeforeEach
 
 
     @Test
     public void Poster() {
 
-        AddingPosterManager manager = new AddingPosterManager();
+        AddingPosterManager manager = new AddingPosterManager();//пустая афиша
 
-            String[] expected = {};
-            String[] actual = manager.findAll();
-        Assertions.assertArrayEquals(expected, actual);
-
-
-    }
-
-   @Test
-    public void addingPoster() {
-
-       AddingPosterManager manager = new AddingPosterManager();
-
-        manager.AddingPosterManager("Бладшот");
-
-
-
-        String[] expected = { "Бладшот" };
+        String[] expected = {};
         String[] actual = manager.findAll();
         Assertions.assertArrayEquals(expected, actual);
 
 
     }
+
+    @Test
+    public void addingPoster1() {//один постер добавился попорядку
+
+        AddingPosterManager manager = new AddingPosterManager();
+
+        manager.AddingPosterManager("Бладшот");
+
+
+        String[] expected = {"Бладшот"};
+        String[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);//?
+
+
+    }
+
+    @Test
+    public void addingPoster2() {//добавилось 5 постеров попорядку
+
+        AddingPosterManager manager = new AddingPosterManager();
+
+        manager.AddingPosterManager("Бладшот");
+        manager.AddingPosterManager("Вперёд");
+        manager.AddingPosterManager("Отель");
+        manager.AddingPosterManager("Джентельмены");
+        manager.AddingPosterManager("Номер один");
+
+        String[] expected = {"Бладшот", "Вперёд", "Отель", "Джентельмены", "Номер один"};
+        String[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+
+
+    }
+
+    @Test
+    public void addingPoster3() {//вывод в обратном порядке по умолчанию (5)
+
+        AddingPosterManager manager = new AddingPosterManager();
+
+        manager.AddingPosterManager("Бладшот");
+        manager.AddingPosterManager("Вперёд");
+        manager.AddingPosterManager("Отель");
+        manager.AddingPosterManager("Джентельмены");
+        manager.AddingPosterManager("Номер один");
+
+        String[] expected = {"Номер один", "Джентельмены", "Отель", "Вперёд", "Бладшот"};
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+
+
+    }
+
+
+    @Test
+    public void addingPoster4() {//вывод постеров в обратном порядке несколько
+
+        AddingPosterManager manager = new AddingPosterManager();
+
+        manager.AddingPosterManager("Бладшот");
+        manager.AddingPosterManager("Вперёд");
+        manager.AddingPosterManager("Отель");
+        manager.AddingPosterManager("Джентельмены");
+
+
+        String[] expected = {"Джентельмены", "Отель", "Вперёд", "Бладшот"};
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+
+
+    }
+
+    @Test
+    public void addingPoster5() {//вывод постеров в обратном порядке несколько
+
+        AddingPosterManager manager = new AddingPosterManager();
+
+        manager.AddingPosterManager("Бладшот");
+        manager.AddingPosterManager("Вперёд");
+        manager.AddingPosterManager("Отель");
+        manager.AddingPosterManager("Джентельмены");
+        manager.AddingPosterManager("Человек невидимка");
+        manager.AddingPosterManager("Тролли");
+
+
+        String[] expected = {"Тролли", "Человек невидимка", "Джентельмены", "Отель", "Вперёд"};
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+
+
+    }
+
 }
